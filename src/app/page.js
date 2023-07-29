25585ffb7ea5b3c4962856e957e995db8501e379
Camilo Hernández <camilohreina.dev@gmail.com>
2023-07-28 19:36:24 -0500

    add search bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-import { MainChart } from "./components/MainChart";
 import { getRecordsByWebSite } from "./services/records";
 import { Card } from "./components/Card.jsx";
+import { SearchBar } from "./components/SearchBar";
+
 export default async function Home() {
   const data = await getRecordsByWebSite({ website: "https://google.com" });
 
   return (
-    <main className="mx-auto containerFluid">
+    <main className="mx-auto gap-8 containerFluid">
+      <SearchBar />
       <div className="w-full my-8 items-stretch grid grid-cols-2 gap-8 m-auto justify-center ">
         {data.map((item) => (
           <Card key={item.key} data={item} />
