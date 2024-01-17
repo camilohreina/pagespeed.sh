2120fbdb59ec1ca2822946816a97b7644c767d23
Camilo Hernandez <camilohreina.dev@gmail.com>
2024-01-16 21:33:50 -0500

    fix metadata

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,13 @@ export function Footer() {
     <footer className="rounded-lg  mt-8  dark:bg-black bg-slate-200">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span className="text-sm text-gray-700 sm:text-center dark:text-gray-400">
-          © 2023{' '}
-          <a href="#" className="hover:underline">
-            Differnt
+          Coded with &#9825; by &nbsp;
+          <a
+            href="https://xdiffernt.com/"
+            className="hover:underline font-bold"
+          >
+            xDiffernt.
           </a>
-          . With Love &#9825;.
         </span>
       </div>
     </footer>
